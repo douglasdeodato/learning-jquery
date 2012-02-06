de82ed399fb695a6c95f315da708b9a5dae79cdd
Douglas Deodato <douglasdeodato@gmail.com>
2012-02-06 21:09:35 +0000

    insert class js and css.

---
+++ tabs/tabs.js
@@ -0,0 +1,10 @@
+$(document).ready(function() { 
+
+	//Default Action
+	$(".tab_conteudo").hide(); //escondendo todo conteudo
+	$("ul.tabs li:first").addClass("active").show(); //Activando a primeira tab
+	$(".tab_conteudo:first").show(); //mostrando conteudo 1 tab
+	
+	
+
+});
