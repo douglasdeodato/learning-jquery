3d8090899ed5aea6a8ab6bef360b6531a8dd2242
Douglas Deodato <douglasdeodato@gmail.com>
2012-02-06 21:10:08 +0000

    commits entenda abaixo.
    linha 1 = escondendo todo conteudo
    linha 2 = ativando a primeira tab
    linha 3 = mostrando conteudo 1 tab.

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
 	//Default Action
 	$(".tab_conteudo").hide(); //escondendo todo conteudo
 	$("ul.tabs li:first").addClass("active").show(); //Activando a primeira tab
-	$(".tab_conteudo:first").show(); //mostrando conteudo 1 tab
-	
-	
+	$(".tab_conteudo:first").show(); //mostrando conteudo 1 ta
 
 });
