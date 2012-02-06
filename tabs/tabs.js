988756776a0ad02b399540400e668bf25119e936
Douglas Deodato <douglasdeodato@gmail.com>
2012-02-06 21:18:41 +0000

    remove comments in jquery file.

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
 	
 	//On Click Event
 	$("ul.tabs li").click(function() {
-		$("ul.tabs li").removeClass("active"); //Remove any "active" class
-		$(this).addClass("active"); //Add "active" class to selected tab
-		$(".tab_conteudo").hide(); //escondendo toda tab conteudo
-		var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + conteudo
-		$(activeTab).fadeIn(); //Fade in the active conteudo
+		$("ul.tabs li").removeClass("active"); 
+		$(this).addClass("active"); 
+		$(".tab_conteudo").hide(); 
+		var activeTab = $(this).find("a").attr("href"); 
+		$(activeTab).fadeIn(); 
 		return false;
 	});
 	
