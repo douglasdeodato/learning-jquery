a7f2a1032ff62be02cbd4778a701972fdae4e85d
Douglas Deodato <douglasdeodato@gmail.com>
2012-02-06 21:11:35 +0000

    remove coments.

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,8 +1,8 @@
 $(document).ready(function() { 
 
 	//Default Action
-	$(".tab_conteudo").hide(); //escondendo todo conteudo
-	$("ul.tabs li:first").addClass("active").show(); //Activando a primeira tab
-	$(".tab_conteudo:first").show(); //mostrando conteudo 1 ta
+	$(".tab_conteudo").hide();
+	$("ul.tabs li:first").addClass("active").show(); 
+	$(".tab_conteudo:first").show(); 
 
 });
