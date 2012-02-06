8f332c3528e507cbcccb12d77e6ef77f787b15d8
Douglas Deodato <douglasdeodato@gmail.com>
2012-02-06 21:12:37 +0000

    entenda mais abaixo. linha1= removendo qualquer class active linha2= adicionando class no selected tab linha3= escondendo o conteudo tab linha4= encontrando rel attribute value para indentificar active tab + conteudo linha5= efeito fade in (active conteudo)

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -4,5 +4,16 @@ $(document).ready(function() {
 	$(".tab_conteudo").hide();
 	$("ul.tabs li:first").addClass("active").show(); 
 	$(".tab_conteudo:first").show(); 
+	
+	//On Click Event
+	$("ul.tabs li").click(function() {
+		$("ul.tabs li").removeClass("active"); //Remove any "active" class
+		$(this).addClass("active"); //Add "active" class to selected tab
+		$(".tab_conteudo").hide(); //escondendo toda tab conteudo
+		var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + conteudo
+		$(activeTab).fadeIn(); //Fade in the active conteudo
+		return false;
+	});
+	
 
 });
