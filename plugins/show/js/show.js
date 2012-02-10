5faa0f08ae960c36e78d503c37a6cb6642905a70
Douglas Deodato <douglasdeodato@gmail.com>
2012-02-10 11:40:45 +0000

    some referrences.

--- plugins/show/js/show.js
+++ plugins/show/js/show.js
@@ -1,3 +1,21 @@
+/*
+                =============================
+                  jQuery Show Plugin
+                            v0.1
+
+                   Demo and Documentation:
+                 no have yet
+                =============================
+
+    A jQuery plugin for Show Div, tables.
+
+    Author: Douglas Deodato (http://www.douglasdeodato.com)
+    Date: 2012-02-10
+
+    Copyright 2012, Douglas Deodato
+    MIT Licensed (http://www.opensource.org/licenses/mit-license.php)
+*/
+
 $(document).ready(function(){
    $('#conteudo').hide(); 
    $('a#exibir').click(function(){
