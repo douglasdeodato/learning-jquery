7c1ecc185865790732640fbd9ce1a0c3c66a392a
Douglas Deodato <douglasdeodato@gmail.com>
2012-02-12 21:15:44 +0000

    create function for see if user put name in inbox if he put nothing if empty so show the label error hide and put focus in input

--- plugins/form/js/script.js
+++ plugins/form/js/script.js
@@ -4,6 +4,15 @@ $(document).ready(function(){
 $('.error').hide();
 $(".button").click(function(){
 	
+	$('.error').hide();
+	var name = $("input#name").val();
+	if (name =="") {
+	$("label#name_error").show();
+	$("input#name").focus();
+	return false;	
+		
+	}
+	
 	
 	
 });
